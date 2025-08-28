972db1e0bbc13b80cbfb379dd8343b18acd4feb9
SADHIN <muhammedsadhin9745@gmail.com>
2025-08-28 15:50:23 +0530

    set up mongo link

--- config.js/db.js
+++ config.js/db.js
@@ -1,9 +1,19 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config(); 
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB connected");
+        const uri = process.env.MONGO_URI;
+
+        if (!uri) {
+          throw new Error(
+            "❌ MONGO_URI is not defined in environment variables"
+          );
+        }
+    await mongoose.connect(uri);
+    console.log("✅ MongoDB connected", uri);
   } catch (error) {
     console.error("❌ MongoDB connection error:", error);
     process.exit(1);
