e8c95a165a0bc1d9f64df9e0a85b7edc04d51bc6
SADHIN <muhammedsadhin9745@gmail.com>
2025-08-28 13:38:53 +0530

    speling change 2

--- server.js
+++ server.js
@@ -45,6 +45,6 @@ app.use("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running  on http://localhost:${PORT}`);
 });
 
