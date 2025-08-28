f340a97e4cb951ab6ce97c7941627c7e17ead814
SADHIN <muhammedsadhin9745@gmail.com>
2025-08-28 16:02:42 +0530

    set up CORS

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:5175",
-      "https://taptune-frontend.vercel.app/",
+      "https://taptune-frontend.vercel.app",
     ],
     credentials: true,
   })
