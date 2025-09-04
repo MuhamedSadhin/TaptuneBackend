5f726f7237b24fef64566a1b06c816cef24efeb6
SADHIN <muhammedsadhin9745@gmail.com>
2025-09-04 14:54:51 +0530

    set token bug

--- controllers/authController.js
+++ controllers/authController.js
@@ -52,10 +52,18 @@ export const loginUser = async (req, res) => {
       expiresIn: "7d",
     });
 
+    // res.cookie("token", token, {
+    //   httpOnly: true,
+    //   secure: process.env.NODE_ENV === "production",
+    //   sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    //   maxAge: 7 * 24 * 60 * 60 * 1000,
+    // });
+
+
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: process.env.NODE_ENV === "production", 
+      sameSite: "None", 
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
