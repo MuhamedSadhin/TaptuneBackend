3c81beccfe7411c2f374fffa2b69cbd3877a162c
SADHIN <muhammedsadhin9745@gmail.com>
2025-09-08 13:25:28 +0530

    set the cookie .;

--- controllers/authController.js
+++ controllers/authController.js
@@ -62,7 +62,7 @@ export const loginUser = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production", 
+      secure: true, 
       sameSite: "None", 
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
